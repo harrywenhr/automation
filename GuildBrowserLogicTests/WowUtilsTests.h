93853c0ce398a7bda90b5d3e1dc5b94aab559a7f
harrywenhr <harrywenhr@gmail.com>
2013-10-12 21:33:09 -0700

    2nd commit

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Yue Wang on 10/12/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
